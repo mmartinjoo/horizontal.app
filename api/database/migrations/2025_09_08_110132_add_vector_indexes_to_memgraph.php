b44d6af79f047751ff9f7c50ba7e23ab02fcb9c0
martinjoo <martin@martinjoo.dev>
2025-09-17 19:59:39 +0200

    Add vector indexes

--- api/database/migrations/2025_09_08_110132_add_vector_indexes_to_memgraph.php
+++ api/database/migrations/2025_09_08_110132_add_vector_indexes_to_memgraph.php
@@ -7,16 +7,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-//        $graphDB = app(GraphDB::class);
-//        $graphDB->run("
-//            CREATE VECTOR INDEX vector_index_entity
-//            ON :__Entity__(embedding)
-//            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000};"
-//        );
-//        $graphDB->run("
-//            CREATE VECTOR INDEX vector_index_communities
-//            ON :Community(embedding)
-//            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000}
-//        ");
+        $graphDB = app(GraphDB::class);
+        $graphDB->run("
+            CREATE VECTOR INDEX vector_index_entity
+            ON :__Entity__(embedding)
+            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000};"
+        );
+        $graphDB->run("
+            CREATE VECTOR INDEX vector_index_communities
+            ON :Community(embedding)
+            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000}
+        ");
     }
 };
