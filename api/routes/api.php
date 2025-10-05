304d62cb165206c1d9cb6f2a54d4a99f1b128882
martinjoo <martin@martinjoo.dev>
2025-10-05 11:00:48 +0200

    WIP: serverless infra

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,5 +6,8 @@ use Illuminate\Support\Facades\Route;
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
         Route::get('/tenants/{tenant}/', [TenantController::class, 'show']);
+        Route::get('/tenants', function () {
+            return \App\Models\Tenant::all();
+        });
     });
 }
