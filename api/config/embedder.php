f4bce13bfc2102edfb25ee562db2d344e8419cfc
martinjoo <martin@martinjoo.dev>
2025-10-06 14:47:33 +0200

    Add secrets

--- api/config/embedder.php
+++ api/config/embedder.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    'provider' => env('EMBEDDER_PROVIDER', 'openai'),
     'model' => env('EMBEDDER_MODEL', 'text-embedding-3-large'),
-    'max_input_tokens' => env('EMBEDDER_MAX_INPUT_TOKENS', 1024),
 ];
