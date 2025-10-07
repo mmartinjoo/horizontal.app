f6b91a9dfd7ae12837564c3165e88e040a4908b7
martinjoo <martin@martinjoo.dev>
2025-10-07 09:34:53 +0200

    Fix memgraph creation logic when creating a tenant

--- api/app/Providers/TenancyServiceProvider.php
+++ api/app/Providers/TenancyServiceProvider.php
@@ -26,9 +26,9 @@ class TenancyServiceProvider extends ServiceProvider
             Events\CreatingTenant::class => [],
             Events\TenantCreated::class => [
                 JobPipeline::make([
+                    SetupGraphDBInstance::class,
                     Jobs\CreateDatabase::class,
                     Jobs\MigrateDatabase::class,
-                    SetupGraphDBInstance::class,
                     // Jobs\SeedDatabase::class,
 
                     // Your own jobs to prepare the tenant.
--- api/database/migrations/tenant/2025_09_08_110132_add_vector_indexes_to_memgraph.php
+++ api/database/migrations/tenant/2025_09_08_110132_add_vector_indexes_to_memgraph.php
@@ -2,22 +2,32 @@
 
 use App\Services\GraphDB\GraphDB;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        $graphDB = app(GraphDB::class);
-        $graphDB->run("
-            CREATE VECTOR INDEX vector_index_entity
-            ON :__Entity__(embedding)
-            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000};"
-        );
-        $graphDB->run("
-            CREATE VECTOR INDEX vector_index_communities
-            ON :Community(embedding)
-            WITH CONFIG {\"dimension\": 768, \"capacity\": 20000}
-        ");
+        try {
+            $graphDB = app(GraphDB::class);
+            $graphDB->run("
+                CREATE VECTOR INDEX vector_index_entity
+                ON :__Entity__(embedding)
+                WITH CONFIG {\"dimension\": 768, \"capacity\": 20000};"
+            );
+            $graphDB->run("
+                CREATE VECTOR INDEX vector_index_communities
+                ON :Community(embedding)
+                WITH CONFIG {\"dimension\": 768, \"capacity\": 20000}
+            ");
+        } catch (Throwable $e) {
+            if (Str::contains($e->getMessage(), 'Given vector index already exists.')) {
+                // this can happen in local or testing environment where we have a fixed number of GraphDB
+                // instances and they are not re-created for each tenant so indexes will exist
+                return;
+            }
+            throw $e;
+        }
     }
 
     public function down()
