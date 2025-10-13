219066841632a752eb28631b47b57d663f6b2423
martinjoo <martin@martinjoo.dev>
2025-10-13 11:01:34 +0200

    Change memgraph storage mode while indexing

--- graphbuilder/src/graphbuilder.py
+++ graphbuilder/src/graphbuilder.py
@@ -1,7 +1,8 @@
 import logging
 from psycopg2.extensions import cursor as Cursor
 from .jobs.index_batch import index_batch
-from src.factories import create_queue, create_db_cursor
+from .jobs.indexing_finished import indexing_finished
+from src.factories import create_queue, create_db_cursor, create_graph_client
 from src.services import count_waiting_comments, count_waiting_document_chunks
 
 class GraphBuilder:
@@ -12,6 +13,12 @@ class GraphBuilder:
         self.queue = create_queue()
         
     def build_graph_for_tenant(self):
+        # This makes possible to run queries in parallel without conflicts
+        # Storage mode is set back to IN_MEMORY_TRANSACTIONAL oncxe all jobs have been finished
+        graph_client = create_graph_client(tenant_id=self.tenant_id)
+        with graph_client.session() as session:
+            session.run("STORAGE MODE IN_MEMORY_ANALYTICAL")
+            
         cursor = create_db_cursor(tenant_id=self.tenant_id)
         self.build_graph(type="comment",
                          cursor=cursor)
@@ -40,10 +47,17 @@ class GraphBuilder:
         logging.info(f"Number of {type}s to process: {count}")
         logging.info(f"Number of batches: {num_of_batches}")
         
+        job_ids = []
         for i in range(num_of_batches):
-            self.queue.enqueue(index_batch,
+            job = self.queue.enqueue(index_batch,
                                type,
                                5,   # limit
                                num_of_batches,
                                i+1, # batch serial
                                self.tenant_id)
+
+            job_ids.append(job.id)
+            
+        self.queue.enqueue(indexing_finished,
+                           tenant_id=self.tenant_id,
+                           depends_on=job_ids)
