05e3ef1e73c046973fc434c46a7de2f779683895
martinjoo <martin@martinjoo.dev>
2025-10-25 21:49:27 +0200

    Add indexing workflow

---
+++ api/database/migrations/tenant/2025_08_23_210502_create_indexing_workflows_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('indexing_workflows', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('started_at');
+            $table->dateTime('finished_at');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('indexing_workflows');
+    }
+};
--- api/database/migrations/tenant/2025_08_23_210602_create_indexing_workflow_steps_table.php
+++ api/database/migrations/tenant/2025_08_23_210602_create_indexing_workflow_steps_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\IndexingWorkflow;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,6 +11,7 @@ return new class extends Migration
     {
         Schema::create('indexing_workflow_steps', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(IndexingWorkflow::class)->constrained()->cascadeOnDelete();
             $table->string('integration');
             $table->integer('overall_items')->default(0);
             $table->string('status');
