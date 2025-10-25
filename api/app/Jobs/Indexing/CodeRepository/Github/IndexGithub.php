c87baea3bca8051f69fe546d6b4f2251562aef26
martinjoo <martin@martinjoo.dev>
2025-10-25 20:01:58 +0200

    Refactor the entire flow

--- api/app/Jobs/Indexing/CodeRepository/Github/IndexGithub.php
+++ api/app/Jobs/Indexing/CodeRepository/Github/IndexGithub.php
@@ -2,15 +2,11 @@
 
 namespace App\Jobs\Indexing\CodeRepository\GitHub;
 
-use App\Jobs\Indexing\CodeRepository\IndexIssue;
-use App\Jobs\Indexing\CodeRepository\IndexPullRequest;
-use App\Models\Document;
+use App\Jobs\Indexing\CodeRepository\IndexIssues;
+use App\Jobs\Indexing\CodeRepository\IndexPullRequests;
 use App\Models\IndexingWorkflow;
-use App\Services\Integration\CodeRepository\DataTransferObjects\Issue;
 use App\Services\Integration\CodeRepository\DataTransferObjects\Repository;
-use App\Services\Integration\CodeRepository\DataTransferObjects\PullRequest;
 use App\Services\Integration\CodeRepository\GitHub\GitHub;
-use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\LazyCollection;
@@ -34,78 +30,8 @@ class IndexGitHub implements ShouldQueue
 
         /** @var Repository $repo */
         foreach ($repositories as $repo) {
-            $pullRequests = $github->pullRequests($repo);
-            $issues = $github->issues($repo);
-
-            $indexing->increment('overall_items', count($pullRequests)+count($issues));
-
-            /** @var PullRequest $pullRequest */
-            foreach ($pullRequests as $pullRequest) {
-                if (!$this->pullRequestNeedsIndexing($pullRequest)) {
-                    $indexing->increment('skipped_items', 1);
-                    continue;
-                }
-
-                // Delete existing document if it exists
-                $count = Document::query()
-                    ->where('source_type', 'github_pr')
-                    ->where('source_id', $pullRequest->id)
-                    ->delete();
-
-                $indexing->increment('deleted_items', $count);
-                IndexPullRequest::dispatch($pullRequest, $github, $indexing->id);
-            }
-
-            /** @var Issue $issue */
-            foreach ($issues as $issue) {
-                if (!$this->issueNeedsIndexing($issue)) {
-                    $indexing->increment('skipped_items', 1);
-                    continue;
-                }
-
-                // Delete existing document if it exists
-                $count = Document::query()
-                    ->where('source_type', 'github_issue')
-                    ->where('source_id', $issue->externalId)
-                    ->delete();
-
-                $indexing->increment('deleted_items', $count);
-                IndexIssue::dispatch($issue, $indexing->id);
-            }
+            IndexPullRequests::dispatch($repo, $github, $indexing->id);
+            IndexIssues::dispatch($repo, $github, $indexing->id);
         }
-
-        $indexing->update(['status' => 'completed']);
-    }
-
-    private function pullRequestNeedsIndexing(PullRequest $pullRequest): bool
-    {
-        $existingContent = Document::query()
-            ->where('source_id', $pullRequest->id)
-            ->where('source_type', 'github_pr')
-            ->first();
-
-        if ($existingContent === null) {
-            return true;
-        }
-
-        return $pullRequest->updatedAt->gt(
-            $existingContent->indexed_at ?? Carbon::parse('1900-01-01 00:00:00')
-        );
-    }
-
-    private function issueNeedsIndexing(Issue $issue): bool
-    {
-        $existingContent = Document::query()
-            ->where('source_id', $issue->externalId)
-            ->where('source_type', 'github_pr')
-            ->first();
-
-        if ($existingContent === null) {
-            return true;
-        }
-
-        return $issue->createdAt->gt(
-            $existingContent->indexed_at ?? Carbon::parse('1900-01-01 00:00:00')
-        );
     }
 }
