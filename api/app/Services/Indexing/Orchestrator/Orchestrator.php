3c9b33f742ea2f4b126592efca7c3ef79da06fda
martinjoo <martin@martinjoo.dev>
2025-10-26 14:34:11 +0100

    .

--- api/app/Services/Indexing/Orchestrator/Orchestrator.php
+++ api/app/Services/Indexing/Orchestrator/Orchestrator.php
@@ -24,7 +24,7 @@ class Orchestrator
         ]);
 
         // This will be merged into one `integrations` table
-        $integrations = ['slack'];
+        $integrations = ['slack', 'google_drive', 'github'];
         $jobs = [];
 
         foreach ($integrations as $integration) {
