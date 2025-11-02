969740377c0653d052ebf15a4136ecb7ee8dc169
martinjoo <martin@martinjoo.dev>
2025-11-02 19:57:52 +0100

    Add basic FE component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/ask',
+      name: 'ask',
+      component: () => import('../views/Ask.vue')
+    }
+  ],
 })
 
 export default router
