324f44373c5526d56f0c297c0f277ea60f06a121
martinjoo <martin@martinjoo.dev>
2025-11-11 09:32:37 +0100

    Use authorized resources in Slack

--- api/app/Services/Indexing/Orchestrator/Orchestrator.php
+++ api/app/Services/Indexing/Orchestrator/Orchestrator.php
@@ -31,7 +31,7 @@ class Orchestrator
         ]);
 
         // This will be merged into one `integrations` table
-        $integrations = ['google_drive', 'slack', 'linear', 'github'];
+        $integrations = ['slack'];
         $jobs = [];
 
         foreach ($integrations as $integration) {
