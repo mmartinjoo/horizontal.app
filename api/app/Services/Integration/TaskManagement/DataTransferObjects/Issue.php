51daa55df245fad2d72769edec77a1031773853b
martinjoo <martin@martinjoo.dev>
2025-11-13 23:00:17 +0100

    Store issue status

--- api/app/Services/Integration/TaskManagement/DataTransferObjects/Issue.php
+++ api/app/Services/Integration/TaskManagement/DataTransferObjects/Issue.php
@@ -12,11 +12,11 @@ class Issue
         public string $title,
         public string $description,
         public string $assignee,
+        public string $status,
         public string $url,
         public Carbon $createdAt,
         public Carbon $updatedAt,
-    ) {
-    }
+    ) {}
 
     public function getLastUpdatedAt(): Carbon
     {
@@ -31,6 +31,7 @@ class Issue
             title: Arr::get($data, 'fields.summary', ''),
             description: $description,
             assignee: Arr::get($data, 'fields.assignee.displayName', ''),
+            status: strtolower(Arr::get($data, 'fields.status.name', '')),
             url: Arr::get($data, 'self'),
             createdAt: Carbon::parse(Arr::get($data, 'fields.created', '1900-01-01T00:00:00.000+0000')),
             updatedAt: Carbon::parse(Arr::get($data, 'fields.updated', '1900-01-01T00:00:00.000+0000')),
@@ -43,7 +44,8 @@ class Issue
             id: Arr::get($data, 'identifier', ''),
             title: Arr::get($data, 'title', ''),
             description: $description,
-            assignee: Arr::get($data, 'assignee.displayName', ''),
+            assignee: Arr::get($data, 'assignee', ''),
+            status: strtolower(Arr::get($data, 'state', '')),
             url: Arr::get($data, 'url', ''),
             createdAt: Carbon::parse(Arr::get($data, 'createdAt', '1900-01-01T00:00:00.000Z')),
             updatedAt: Carbon::parse(Arr::get($data, 'updatedAt', '1900-01-01T00:00:00.000Z')),
@@ -54,8 +56,9 @@ class Issue
     {
         $str = '';
         foreach ($this as $key => $value) {
-            $str .= $key . ':' . $value . ' ';
+            $str .= $key.':'.$value.' ';
         }
+
         return $str;
     }
 }
