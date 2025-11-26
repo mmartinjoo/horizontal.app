83de4c3884e11fe2de728cda60f1965ddb1d69ac
martinjoo <martin@martinjoo.dev>
2025-11-26 23:25:39 +0100

    Fix start script

--- infra/memgraph/start.sh
+++ infra/memgraph/start.sh
@@ -5,7 +5,7 @@ export MEMGRAPH1_PASSWORD=
 export MEMGRAPH2_USER=
 export MEMGRAPH2_PASSWORD=
 
-echo "vm.max_map_count=262144" >> /etc/sysctl.d/99-memgraph.conf
+echo "vm.max_map_count=262144" > /etc/sysctl.d/99-memgraph.conf
 sudo sysctl -p /etc/sysctl.d/99-memgraph.conf
 
 docker compose down
