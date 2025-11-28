5d6fad4e10ab23d0a26ad3c5a9e2bab8ee816605
martinjoo <martin@martinjoo.dev>
2025-11-28 01:07:56 +0100

    Fix DB is not available problem

--- api/routes/console.php
+++ api/routes/console.php
@@ -11,36 +11,45 @@ use App\Jobs\Indexing\TaskManagement\Linear\RefreshLinearTokensJob;
 use App\Jobs\Infra\SuperviseAvailableMemgraphInstances;
 use App\Jobs\LLM\RotateLLMProvider;
 use App\Models\Tenant;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
-foreach (Tenant::all() as $tenant) {
-    Schedule::job(new RefreshJiraTokensJob($tenant))->everyThirtyMinutes();
-    Schedule::job(new RefreshLinearTokensJob($tenant))->everyThirtyMinutes();
-    Schedule::job(new RefreshSlackTokensJob($tenant))->everyThirtyMinutes();
-    Schedule::job(new RefreshGoogleDriveTokensJob($tenant))->everyThirtyMinutes();
-    Schedule::job(new RefreshGoogleChatTokensJob($tenant))->everyThirtyMinutes();    
-
-    Schedule::job(new UpdateSlackMessageLinks($tenant))->everyFiveMinutes();
-
-    // the server is in UTC. when it's 3AM:
-    //  - 7PM-11PM (previous day) in the US
-    //  - 1AM-4AM in Europe
-    Schedule::job(new StartIndexing($tenant))->dailyAt('03:00');
-    Schedule::job(new StartEmergencyIndexing($tenant))->everyThirtyMinutes();
-
-    Schedule::job(new RotateLLMProvider($tenant))->everyFifteenMinutes();
-
-    $integration = [
-        'google_drive',
-        'github',
-        'slack',
-        'linear',
-        'google_chat',
-        'jira',
-    ];
-    foreach ($integration as $integration) {
-        Schedule::command("integrations:check-refresh-tokens --tenant={$tenant->id} --integration={$integration} --show-details")->hourly();
-    }
-}
-
 Schedule::job(new SuperviseAvailableMemgraphInstances)->everyMinute();
+
+// tenant-aware jobs
+// it needs the database check because `composer dump-autoload` invokes it
+// which is executed in the Dockerfile
+try {
+    DB::connection()->getPdo();
+
+    foreach (Tenant::all() as $tenant) {
+        Schedule::job(new RefreshJiraTokensJob($tenant))->everyThirtyMinutes();
+        Schedule::job(new RefreshLinearTokensJob($tenant))->everyThirtyMinutes();
+        Schedule::job(new RefreshSlackTokensJob($tenant))->everyThirtyMinutes();
+        Schedule::job(new RefreshGoogleDriveTokensJob($tenant))->everyThirtyMinutes();
+        Schedule::job(new RefreshGoogleChatTokensJob($tenant))->everyThirtyMinutes();    
+
+        Schedule::job(new UpdateSlackMessageLinks($tenant))->everyFiveMinutes();
+
+        // the server is in UTC. when it's 3AM:
+        //  - 7PM-11PM (previous day) in the US
+        //  - 1AM-4AM in Europe
+        Schedule::job(new StartIndexing($tenant))->dailyAt('03:00');
+        Schedule::job(new StartEmergencyIndexing($tenant))->everyThirtyMinutes();
+
+        Schedule::job(new RotateLLMProvider($tenant))->everyFifteenMinutes();
+
+        $integration = [
+            'google_drive',
+            'github',
+            'slack',
+            'linear',
+            'google_chat',
+            'jira',
+        ];
+        foreach ($integration as $integration) {
+            Schedule::command("integrations:check-refresh-tokens --tenant={$tenant->id} --integration={$integration} --show-details")->hourly();
+        }
+    }
+} catch (Exception $e) {
+}
