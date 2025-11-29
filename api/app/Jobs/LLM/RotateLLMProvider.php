d824f41786ca45c1ccf829dfc354bf7a95aec426
martinjoo <martin@martinjoo.dev>
2025-11-29 16:17:01 +0100

    Remove debug logs

--- api/app/Jobs/LLM/RotateLLMProvider.php
+++ api/app/Jobs/LLM/RotateLLMProvider.php
@@ -6,7 +6,6 @@ use App\Models\Tenant;
 use App\Services\LLM\LLMRotation;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use League\OAuth1\Client\Server\Xing;
 
 class RotateLLMProvider implements ShouldQueue
 {
@@ -18,18 +17,9 @@ class RotateLLMProvider implements ShouldQueue
 
     public function handle(LLMRotation $llmRotation)
     {
-        logger()->info('---- Rotate LLM provider ----');
-        logger()->info('tenant BEFORE update');
-        logger()->info($this->tenant);
         $currentProvider = $this->tenant->llm_provider ?? config('llm.default');
-        logger()->info('current provider');
-        logger()->info($currentProvider);
         $newProvider = $llmRotation->rotate($currentProvider);
-        logger()->info('new provider');
-        logger()->info($newProvider);
         $this->tenant->llm_provider = $newProvider;
         $this->tenant->save();
-        logger()->info('tenant AFTER update');
-        logger()->info($this->tenant);
     }
 }
